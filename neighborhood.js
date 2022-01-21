6c6ebe35610794ee6f0db72bbc8c4d60300e3cf9
juanskysword <juanskysword@gmail.com>
2022-01-21 12:25:57 -0600

    Final

---
+++ neighborhood.js
@@ -0,0 +1,9 @@
+const rand = document.getElementById('random');
+rand.addEventListener("click", randomize);
+
+function randomize(event){
+    alert("")
+    console.log('click')
+}
+
+var myArray = [];
