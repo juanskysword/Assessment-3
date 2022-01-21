d5fb2d1282576508a51b125accd97d28f440ba2b
juanskysword <juanskysword@gmail.com>
2022-01-21 10:32:28 -0600

    part4

--- about.js
+++ about.js
@@ -6,9 +6,15 @@ function handleSubmit(evt) {
 	evt.preventDefault();
 	
 	console.log('form submit');
+	alert("form has been Submitted");
 }
 
 
 let form = document.querySelector('form#contact');
 
 form.addEventListener('submit', handleSubmit);
+
+ function secret (){
+	 alert("There are 5 Duckiesss Awesome User")
+	 console.log("MouseOver")
+ }
