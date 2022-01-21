964f88eac7adcb1942626d75eff323ab912a0007
juanskysword <juanskysword@gmail.com>
2022-01-21 12:05:16 -0600

    Finishworkingonextracredit

---
+++ profile.js
@@ -0,0 +1,27 @@
+function btn1 (event) {
+    console.log('Button Clicked');
+    alert("DarkBlue")
+ }
+ 
+
+const color = document.getElementById("color");
+color.addEventListener('click', btn1);
+
+function btn2 (event) {
+    console.log('Button Clicked');
+    alert("Favorite Place Would be Mexico")
+ }
+
+ const place = document.getElementById("place")
+ place.addEventListener('click', btn2)
+
+
+ function btn3 (event) {
+    console.log('Button Clicked');
+    alert("I have a funny Ritual i do ingames, for lootboxes i just go to nice pretty places and open my packs in there for goodluck")
+ }
+
+ const ritual = document.getElementById("ritual")
+ ritual.addEventListener('click', btn3)
+
+ 
